a751910dcbe2ef9e3f3f27cd17314e37827339e0
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-03-22 18:02:57 +0530

    pushing arrays folder and few programs as my first commit

---
+++ Arrays/printEle.js
@@ -0,0 +1,7 @@
+//print the elements in array
+
+const arr = [10, 20, 30, 40, 50];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
---
+++ Arrays/sumOfEle.js
@@ -0,0 +1,10 @@
+//Find the sum of all elements.
+
+const arr = [10, 20, 30, 40, 50];
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+
+console.log("Sum of all elements:", sum);
